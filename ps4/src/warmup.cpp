80914e88fab564a3b7f2993df516d8011284c9d1
KruthikaHassan <kruthika.ha@gmail.com>
2018-04-30 18:35:28 -0700

    problem set 4

---
+++ ps4/src/warmup.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+//#include "Vector.hpp"
+#include "Matrix.hpp"
+//#include "amath583.hpp"
+//#include "amath583.cpp"
+
+using namespace std;
+
+int main{
+
+	RowMatrix A(2,3);
+//	randomize(ColMatrix A);
+
+/*	for(int i=0; i < A.num_rows; ++i){
+		for(int j=0; j < A.num_cols; ++j){
+			cout << A(i,j) << endl;
+		}
+		cout << "\t" << endl;
+	}*/
+
+	return 0;
+}
