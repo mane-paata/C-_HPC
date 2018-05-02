3c588fe9b665abefe39c51f3d844ef3d11c8710a
Abhijith Jagannath <ajagannath@brightcove.com>
2018-05-01 20:46:24 -0700

    update

--- ps4/grader/candidate/square_root.cpp
+++ ps4/grader/candidate/square_root.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
 #include "amath583IO.hpp"
 
 using namespace std;
@@ -27,24 +26,6 @@ int main(int argc, char *argv[])
   complex<double> in = readComplexDouble(argv[1]);
 
   writeComplexDouble(sqrt583(in), "square_root.txt");
-=======
-#include <cmath>
-#include <complex>
-
-using namespace std;
-
-int main(int argc, char* argv[])
-{
-	if(argc != 2)
-  {
-    cout << "Usage: " << argv[0] << "[ComplexDoubleObjectFile]"  << endl;
-    return -1;
-  }
-
-	complex<double> z = readComplexDouble(argv[1]);
-
-	writeComplexDouble(sqr_root, "square_root.txt");
->>>>>>> bffead89637db5c6f65f6c136a78af0de2e8dc88
 
   return 0;
 }
