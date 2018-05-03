1fe29654878b5bc9f5d51e45d8840b221b3f8e35
Abhijith Jagannath <ajagannath@brightcove.com>
2018-05-02 21:41:20 -0700

    final for ps4

--- ps4/grader/candidate/plot_t.py
+++ ps4/grader/candidate/plot_t.py
@@ -19,22 +19,24 @@ for name in sys.argv[1:]:
     with open(infile, 'r') as csvfile:
         Ns = []
         Flops = []
+        Time = []
         
         reader = csv.DictReader(csvfile, delimiter='\t')
     
         for row in reader:
             Ns.append(int(row['N']))
             Flops.append(float(row['Flops'])/1.e9)
+            Time.append(float(row['Time'])/1.e3)
 
-        plt.semilogx(Ns, Flops)
+        plt.semilogx(Ns, Time)
 
-pp = PdfPages('RowMatrix_GFlops.pdf');
+pp = PdfPages('RowMatrix_Time.pdf');
 
-plt.axis([2, 2048, .01, 6])
+plt.axis([2, 2048, 0, 400])
 plt.legend(names)
 plt.title('RowMatrix RowMatrix Product Performance')
 plt.xlabel('Matrix Dimension')
-plt.ylabel('GFlops')
+plt.ylabel('Time (seconds)')
 plt.xticks([2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048] , [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048])
 
 pp.savefig()
